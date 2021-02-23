e7ab96c5d07912a13a44eb5288c1b712b2007c17
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-02-23 10:59:08 -0500

    i#

--- controller/tinycrypt-master/tests/test_hmac.c
+++ controller/tinycrypt-master/tests/test_hmac.c
@@ -354,7 +354,7 @@ int main(void)
 
         show_str1("Digest", digest, sizeof(digest));
 
-        const uint8_t data1[] = "helloooo";
+        const uint8_t data1[8] = "helloooo";
 
         (void)memset(&h, 0x00, sizeof(h));
         (void)tc_hmac_set_key(&h, key, sizeof(key));
