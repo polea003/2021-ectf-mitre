b5611877da8d0af24692fc54a77a612bbc0b6dcc
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-02-22 22:05:36 -0500

    i#

--- controller/tinycrypt-master/tests/test_cbc_mode.c
+++ controller/tinycrypt-master/tests/test_cbc_mode.c
@@ -86,7 +86,7 @@ const uint8_t iv[16] = {
 	0x0c, 0x0d, 0x0e, 0x0f
 };
 
-const uint8_t plaintext[16000] = { "0123456789abcdef hello there" };
+const uint8_t plaintext[55] = { "0123456789abcdef hello there" };
 
 const uint8_t ciphertext[80] = {
 	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b,
