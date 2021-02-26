f32aa5de7ba0608618c11946b780849756fef38d
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-02-26 12:46:20 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -139,7 +139,7 @@ int handle_scewl_recv(char* data, scewl_id_t src_id, uint16_t len) {
 	tc_cbc_mode_decrypt(decrypted, length, (uint8_t *)p, length, (uint8_t *)data, &a);
   send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, len, (char *)decrypted);
 */
-  return send_msg(CPU_INTF, src_id, SCEWL_ID, len, (char *)decrypted);
+  return send_msg(CPU_INTF, src_id, SCEWL_ID, len, data;
 }
 
 
