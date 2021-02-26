aa37ca97bf70715b7778ff0bd1564f167bca15c2
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-02-26 11:27:56 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -128,7 +128,16 @@ int send_msg(intf_t *intf, scewl_id_t src_id, scewl_id_t tgt_id, uint16_t len, c
 
 
 int handle_scewl_recv(char* data, scewl_id_t src_id, uint16_t len) {
-  return send_msg(CPU_INTF, src_id, SCEWL_ID, len, data);
+  	uint8_t decrypted[len];
+    	uint8_t *p;
+	unsigned int length;
+struct tc_aes_key_sched_struct a;
+	(void)tc_aes128_set_decrypt_key(&a, key);
+	p = &data[16];
+	length = ((unsigned int) len);
+	tc_cbc_mode_decrypt(decrypted, length, p, length, data, &a);
+
+  return send_msg(CPU_INTF, src_id, SCEWL_ID, len, decrypted);
 }
 
 
@@ -136,14 +145,9 @@ int handle_scewl_send(char* data, scewl_id_t tgt_id, uint16_t len) {
     struct tc_aes_key_sched_struct a;
 	uint8_t iv_buffer[16];
 	uint8_t encrypted[len];
-	uint8_t decrypted[len];
-	uint8_t *p;
-	unsigned int length;
-  struct tc_hmac_state_struct h;
-  uint8_t digest[32];
   (void)tc_aes128_set_encrypt_key(&a, key);
-  tc_cbc_mode_encrypt(encrypted, sizeof(data) + 16,
-				data, sizeof(data), iv_buffer, &a);
+  tc_cbc_mode_encrypt(encrypted, len + 16,
+				data, len, iv_buffer, &a);
 
 	(void)memcpy(iv_buffer, iv, 16);
   return send_msg(RAD_INTF, SCEWL_ID, tgt_id, len, encrypted);
