6f09c6d605d1c72f40ea6808033e388d0b5bc22a
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-02-27 08:21:07 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -151,7 +151,7 @@ int handle_scewl_recv(char* data, scewl_id_t src_id, uint16_t len) {
   send_str("calulated HMAC:");
   send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, sizeof(digest), (char *)digest);
 
-  if (_compare(digest, hmac, 32))
+  if (!_compare(digest, hmac, 32))
   {
       send_str("HMAC matches, message authentic. Decrypting");
 
