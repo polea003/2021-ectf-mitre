40a865ba4748ff0eaa06aac5cffebaf044ec4bde
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-05 12:27:01 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -359,8 +359,8 @@ int sss_register() {
   scewl_sss_msg_t msg;
   scewl_id_t src_id, tgt_id;
   int status, len;
-  send_str("SECRET LINE: ");
-  send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, sizeof(SECRET), (char *)SECRET);
+  send_str("Provisioned Secret: ");
+  send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, 16, SECRET);
 
   // fill registration message
   msg.dev_id = SCEWL_ID;
