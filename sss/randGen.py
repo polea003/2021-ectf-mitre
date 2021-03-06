148f00d07eeab3953b23e00c4817f5c3330c7ee2
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-06 17:41:10 -0500

    i#

---
+++ sss/randGen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# 2021 Collegiate eCTF
+# SCEWL Security Server
+# Ben Janis
+#
+# (c) 2021 The MITRE Corporation
+#
+# This source file is part of an example system for MITRE's 2021 Embedded System CTF (eCTF).
+# This code is being provided only for educational purposes for the 2021 MITRE eCTF competition,
+# and may not meet MITRE standards for quality. Use this code at your own risk!
+
+import random
+
+
+a = str(random.randint(0,255))
+f = open("/secrets/data.txt", "w")
+f.write(a)
+f.close()
