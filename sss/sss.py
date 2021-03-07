71023586c8ee67f1af1e1092ba3bf52351a8cf32
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-07 14:07:00 -0500

    i#

--- sss/sss.py
+++ sss/sss.py
@@ -90,20 +90,7 @@ class SSS:
     def start(self):
         unattributed_socks = set()
 
-        logger = logging.getLogger('spam_application')
-        logger.setLevel(logging.DEBUG)
-        fh = logging.FileHandler('spam.log')
-        fh.setLevel(logging.DEBUG)
-        # create console handler with a higher log level
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.ERROR)
-        # create formatter and add it to the handlers
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh.setFormatter(formatter)
-        ch.setFormatter(formatter)
-        # add the handlers to the logger
-        logger.addHandler(fh)
-        logger.addHandler(ch)
+
 
         # serve forever
         while True:
