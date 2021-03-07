d00884334258742b567f7db00c624918a550bc96
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-07 13:06:34 -0500

    i#

--- sss/randGenSED.py
+++ sss/randGenSED.py
@@ -14,7 +14,7 @@ import secrets
 
 secretsGen = secrets.SystemRandom()
 
-a = str(secretsGen._randbelow(255))
+a = str(secretsGen._randbelow(4294967295))
 f = open("/secrets/data1", "w")
 f.write(a)
 f.close()
