87c6af8c2d662e92d933475b08e94b15045e2a83
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-07 14:35:20 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -403,7 +403,7 @@ int handle_registration(char* msg) {
 
 
 int sss_register() {
-  char msg2[sizeof(scewl_sss_msg_t) + 16];
+  char msg2[20];
   scewl_sss_msg_t msg;
   scewl_id_t src_id, tgt_id;
   int status, len;
