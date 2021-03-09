a9152f50d7becd4184fff65c744fffca162116d9
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-09 15:41:20 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -197,14 +197,13 @@ int handle_scewl_recv(char* data, scewl_id_t src_id, uint16_t len) {
           return 0; 
           }
       } 
-      send_str("Calculated HMAC:");
-      send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, 32 , (char *)digest); 
 
-      for (int i = 0; i < 32; i ++) {
-        DTdigestArray[2][i] = DTdigestArray[1][i];
-        DTdigestArray[1][i] = DTdigestArray[0][i];
-        DTdigestArray[0][i] = digest[i];
+      for (int j = 0; i < 2; j++) {
+        for (int i = 0; i < 32; i++) {
+          DTdigestArray[j+1][i] = DTdigestArray[j][i];
+        }
       }
+      for (int i = 0; i < 32; i++) DTdigestArray[0] = digest[i];
 
       uint16_t sizeofDec = n - 16;
       uint8_t decrypted[sizeofDec]; //create decryted text array
@@ -496,6 +495,10 @@ int sss_deregister() {
     iv[i] = badKey[i]; 
   }
 
+  for (int i = 0; i < 3; i++) {
+  send_str("HMAC list:");
+  send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, 32 , (char *)DTdigestArray[i]); 
+  }
 
   // receive response
   len = read_msg(SSS_INTF, (char *)&msg, &src_id, &tgt_id, sizeof(scewl_sss_msg_t), 1);
