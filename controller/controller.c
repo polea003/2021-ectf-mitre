cdb33b5b22f076fae87b4ce856560b7d4962a7c8
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-12 08:03:35 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -100,12 +100,8 @@ int read_msg(intf_t *intf, char *data, scewl_id_t *src_id, scewl_id_t *tgt_id,
              size_t n, int blocking) {
   
   data[SCEWL_MAX_DATA_SZ - 1] = '\0';
-  char tempAry[10];
-  send_str("strlen buf:");
-  send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, 10 , itoa((unsigned long)strlen(data), tempAry, 10));
 
   if (strlen(data) > (SCEWL_MAX_DATA_SZ - 32)) {
-    for (int i = 0; i < strlen(data); i++) intf_readb(intf, 0);
     send_str("disgarding message because too long");
     n = 0; 
   }
