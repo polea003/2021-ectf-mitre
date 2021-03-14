be676ca83a582de4f7ed6522779b7d848e427bc6
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-13 19:35:56 -0500

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -53,16 +53,15 @@ int registered = 0;
 int read_msg(intf_t *intf, char *data, scewl_id_t *src_id, scewl_id_t *tgt_id,
              size_t n, int blocking) {
 
-  
-
   data[SCEWL_MAX_DATA_SZ - 1] = '\0'; //set last character equal to terminating value
   if (strlen(data) > 16456) {   
-  for (int i = 0; i < SCEWL_MAX_DATA_SZ; i++) {
-    intf_readb(intf, 0);
+  for (int i = (SCEWL_MAX_DATA_SZ - 1) ; i >= 0; i--) {
     data[i] = '\0';
+    intf_readb(intf, 0);
   }
     bufFlag = 1;
   }
+  
 
   scewl_hdr_t hdr;
   int read, max;
