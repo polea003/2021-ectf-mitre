213450a46630ec276791eb2cd86f7c50e3b78664
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-14 21:57:28 -0400

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -197,6 +197,9 @@ int handle_scewl_recv(char* data, scewl_id_t src_id, uint16_t len) {
 }
 
 int handle_scewl_send(char* data, scewl_id_t tgt_id, uint16_t len) {
+
+  send_str("recieved message:");
+  send_msg(RAD_INTF, SCEWL_ID, SCEWL_FAA_ID, len, data);
   
   //check if message exceeds buffer length and shorten if necessary
  /* if (len > maxMsgLength) {
