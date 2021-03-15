d847fe270340a23da0f6fdd271a1a5b8edfe18cb
pwoleary <69978971+pwoleary@users.noreply.github.com>
2021-03-14 22:41:30 -0400

    i#

--- controller/controller.c
+++ controller/controller.c
@@ -199,8 +199,8 @@ int handle_scewl_send(char* data, scewl_id_t tgt_id, uint16_t len) {
   //check if message exceeds buffer length and reduces it to a nominal length if so
   if (len > maxMsgLength) {
     memset(data, 0, len);
-    data = "abcdef0123456789";
-    len = 16;
+    data = "abcdef0123456789abcdef0123456789";
+    len = 32;
   } 
 
   msgCounter++; //increment message counter for unique message ID
